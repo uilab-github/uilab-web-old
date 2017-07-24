a7ff89f13b90b8463a45a2d56d0a1ac3544abfe6
Jeongmin Byun <jmbyun91@gmail.com>
2017-07-24 19:19:19 +0900

    add admin link to go github repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,21 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="c-footer">
-        <div className="c-footer__contact">
+        <div className="c-footer__body">
+          <div className="c-footer__contact">
             Tel. +82 42 350 7749 <br />
             U&amp;I Lab., Room 4417, E3-1 Computer Science Building, <br />
             KAIST, 291 Daehak-ro, Yuseong-gu, Daejeon 305-701, South Korea
+          </div>
+          <div className="c-footer__links">
+            <a
+              className="c-footer__link"
+              href="https://github.com/uilab-github/uilab-github.github.io"
+              target="_blank"
+            >
+              Admin
+            </a>
+          </div>
         </div>
       </footer>
     );
