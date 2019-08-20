7c766a15683903036a44040d088bc6e7a31e10d1
Jeongmin Byun <jmbyun91@gmail.com>
2019-08-20 11:10:19 +0900

    remove phone number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,6 @@ export default class Footer extends Component {
       <footer className="c-footer">
         <div className="c-footer__body">
           <div className="c-footer__contact">
-            Tel. +82 42 350 7749 <br />
             U&amp;I Lab., Room 4417, E3-1 Computer Science Building, <br />
             KAIST, 291 Daehak-ro, Yuseong-gu, Daejeon 34141, South Korea
           </div>
